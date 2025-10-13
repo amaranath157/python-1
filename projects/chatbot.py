a0fcf9e867a773bad0717d42ab882c1430d8094c
amaranath157 <t.amarnath795@gmail.com>
2025-10-13 17:56:10 +0900

    Our college chatbot

--- projects/chatbot.py
+++ projects/chatbot.py
@@ -1,1 +1,15 @@
-
+while True:
+    user=input("ask u r quetion:")
+    if user.lower():
+        if user in ['hello','hi']:
+            print("bot: hi, how can i help you?")
+        elif user == "courses":
+            print("bot: MITS offered mca,mba,b.tech,m.tech")
+        elif user=="hostel":
+            print("bot: separate hostels is there for girls and boys")
+        elif user == "bye":
+            print("thak u ")
+            break
+        else:
+            print("i dont understand")
+            break
